787a0aae212992e6fc2d6ba2ecb2c91e510dc754
LandonLorica5555 <landonlorica41@gmail.com>
2025-07-16 14:35:14 +0800

    first commit

--- src/components/pages/home/skills/Skills.jsx
+++ src/components/pages/home/skills/Skills.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
     <>
       <section className="skills bg-[#393E46] w-full p-20">
         <div className="container">
-            
+          <h1 className="text-white text-4xl font-bold text-center mb-10">
+            My Skills
+          </h1>
+          <div className="grid grid-cols-[1fr_1fr_1fr] gap-6">
+            <div className="bg-white size-80 shadow-xl p-6 rounded-xl">
+
+            </div>
+          </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
